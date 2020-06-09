74b71953cde89345d60ff67adc82adae8c838f01
James Yeates <jimibue@gmail.com>
2020-06-09 10:35:41 -0600

    contact list show working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Container, Header } from "semantic-ui-react";
+import Contacts from "./Contacts";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    contacts: [
+      { id: 1, name: "Sally", phone: "800-911-9111" },
+      { id: 2, name: "Jill", phone: "8675309" },
+      { id: 3, name: "George", phone: "10" },
+    ],
+  };
+  render() {
+    return (
+      <Container>
+        <Header as="h1">React Contact List</Header>
+        <Contacts contacts={this.state.contacts} />
+      </Container>
+    );
+  }
 }
 
 export default App;
